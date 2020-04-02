feaa22a2b29a752445e0d98c7e5da525bd434370
Jefferson Rodrigues <jeffersonrodrigues@Jeffersons-MacBook-Pro.local>
2020-04-02 18:10:03 -0300

    Convertendo dado para String

--- aws/sqs_helper.py
+++ aws/sqs_helper.py
@@ -11,4 +11,5 @@ class SQSHelper:
         queue_url = os.environ['FIFO_QUEUE_NAME']
         message = str(data)
         print(type(message))
+        
         client.send_message(QueueUrl=queue_url, MessageBody=message, MessageGroupId="123456")
