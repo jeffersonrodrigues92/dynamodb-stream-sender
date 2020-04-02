9586ff6981935cc3efac40b3db610070da0bd3aa
Jefferson Rodrigues <jeffersonrodrigues@Jeffersons-MacBook-Pro.local>
2020-04-02 17:16:20 -0300

    Adicionando fifo com boto3

--- aws/sqs_helper.py
+++ aws/sqs_helper.py
@@ -1,5 +1,6 @@
 import boto3
 import uuid
+import json
 import os
 
 client = boto3.client('sqs')
@@ -8,4 +9,4 @@ class SQSHelper:
 
     def send_message(self, data):
         queue_url = os.environ['FIFO_QUEUE_NAME']
-        client.send_message(QueueUrl=queue_url, MessageBody=data, MessageGroupId="123456")
+        client.send_message(QueueUrl=queue_url, MessageBody=json.dumps(data), MessageGroupId="123456")
