21d683415741a11e374b10e3810610a7ed96f64a
Jefferson Rodrigues <jefferson.comff@hotmail.com>
2020-05-21 20:52:08 -0300

    reestruturação

--- handler.py
+++ handler.py
@@ -12,9 +12,38 @@ sqs_helper = SQSHelper()
 def handler(event, context):
 
     datas = event['Records']
+    replica_region = os.environ['REPLICA_REGION']
+    account_region = os.environ['ACCOUNT_REPLICA']
 
     try:
         log.info('Sending Message To Queue with: {}'.format(len(datas)))
-        sqs_helper.send_message_batch(datas)
+        assume_role(account_region, 'SenderStreamSQSRole', replica_region, 'sqs')
+        sqs_helper.send_message_batch(datas,)
     except Exception as exception:
         raise exception
+
+def assume_role(account, role, resource, region):
+
+    try:
+        
+        sts_client = boto3.client('sts')
+        assumed_role_object = sts_client.assume_role(
+            RoleArn="arn:aws:iam::" + account + ":role/" + role,
+            RoleSessionName=role
+        )
+        credentials = assumed_role_object['Credentials']
+        client_resource = boto3.client(
+            resource,
+            region_name=region,
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
+        )
+
+    except Exception as exception:
+        log.error('Error To Assume Role With Exception{}'.format(exception))
+        raise exception
+    
+    log.info('Role Assumed with Succeed')
+    
+    return client_resource
